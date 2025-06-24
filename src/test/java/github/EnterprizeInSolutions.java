8c1104d942aa56d71beff943a423561a9f04220a
Inna Torukalo <iniska2002@gmail.com>
2025-06-24 11:37:59 +0300

    added EnterprizeInSolutions, LoginPO

---
+++ src/test/java/github/EnterprizeInSolutions.java
@@ -0,0 +1,26 @@
+package github;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class EnterprizeInSolutions {
+
+    @BeforeAll
+    static void setUp(){
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
+    }
+
+    @Test
+    void shouldHaveEnterprisesTest() {
+        open("https://github.com/");
+        $(".HeaderMenu-nav").$(byText("Solutions")).hover();
+        $(byText("Enterprises")).click();
+        $("html").shouldHave(text("The AI-powered\ndeveloper platform"));
+    }
+
+}
---
+++ src/test/java/selenide/LoginPO.java
@@ -0,0 +1,26 @@
+package selenide;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class LoginPO {
+
+    public void loginWithPassportaAndCard (String card, String password){
+
+    }
+
+    public void loginWirhUserNameAndPassword (String username, String password){
+        $("#username").setValue(username);
+        $("#password").setValue(password);
+        $("submit").click();
+    }
+
+    public void loginWirhUserNameAndPasswordNegative (String username, String password){
+        $("#username").setValue(username);
+        $("#password").setValue(password);
+        $("submit").click();
+    }
+}
